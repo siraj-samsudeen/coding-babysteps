106427f67a265d3f019d296a1365f7a9c30b5507
Safwan Samsudeen <safwan@Safwans-MacBook-Air.local>
2020-08-28 11:54:20 +0530

    Feature 2 - Prevent empty string

--- todo.js
+++ todo.js
@@ -1,12 +1,24 @@
 const inputElement = document.querySelector("#new-item");
 const todoForm = document.querySelector("form");
 const todoList = document.querySelector("#list");
+const submitButton = document.querySelector("#submit-button");
 
 function addNewItem(e) {
 	todoList.insertAdjacentHTML("beforeend", `<li>${inputElement.value}</li>`);
 	todoForm.reset(); //Clear the already entered value as we are not refreshing the page
 
-	e.preventDefault(); //Refreshing the page will clear away the todoList items. So we need to prevent it
+	// A valid item will enable the save button in the validate function, so we need to disable it
+	submitButton.setAttribute("disabled", "");
+	e.preventDefault(); // Refreshing the page will clear away the todoList items. So we need to prevent it
+}
+
+function preventEmptyItem(e) {
+	if (this.value.trim()) {
+		submitButton.removeAttribute("disabled");
+	} else {
+		submitButton.setAttribute("disabled", "");
+	}
 }
 
 todoForm.addEventListener("submit", addNewItem);
+inputElement.addEventListener("input", preventEmptyItem);
