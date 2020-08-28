6632776e9fd5e2681dc41efed704b09678deb13c
Siraj Samsudeen <mailsiraj@gmail.com>
2020-08-28 08:40:02 +0530

    Adding Test.js for Feature 1

---
+++ test.js
@@ -0,0 +1,8 @@
+/*
+Feature 1
+Create a new to-do item
+
+Tests
+1. Create "Buy Vegetables" and check whether it is saved.
+2. Create "Buy Notebook" and check whether both items are displayed now.
+*/
