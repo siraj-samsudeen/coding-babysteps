dbd1782366fbeb334da15fca1f7bf4de0498e7e6
Siraj Samsudeen <mailsiraj@gmail.com>
2020-08-28 11:52:31 +0530

    Add Feature 2 Tests

--- test.js
+++ test.js
@@ -3,6 +3,13 @@ Feature 1
 Create a new to-do item
 
 Tests
-1. Create "Buy Vegetables" and check whether it is saved.
 2. Create "Buy Notebook" and check whether both items are displayed now.
+
+Feature 2
+Prevent empty string or string with only spaces
+
+Tests
+1. Press Submit without entering anything
+2. Press a few spaces and Submit
+
 */
